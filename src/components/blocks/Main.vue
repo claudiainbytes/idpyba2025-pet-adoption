663b76f860c7d27649703e7d9d78b2788b0119e3
claudiainbytes <claudiaeforero@hotmail.com>
2025-01-09 13:40:51 -0500

    Task_003_Boilerplate

---
+++ src/components/blocks/Main.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import Intro from './../../components/blockComponents/Intro.vue'
+import Card from './../../components/blockComponents/Card.vue'
+</script>
+
+<template>
+    <main>
+        <Intro/>
+            <div class="album py-5 bg-body-tertiary">
+            <div class="container">
+
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col">
+                    <Card/>
+                </div>
+                <div class="col">
+                    <Card/>
+                </div>
+                <div class="col">
+                    <Card/>
+                </div>
+
+                <div class="col">
+                    <Card/>
+                </div>
+                <div class="col">
+                    <Card/>
+                </div>
+                <div class="col">
+                    <Card/>
+                </div>
+
+                <div class="col">
+                    <Card/>
+                </div>
+                <div class="col">
+                    <Card/>
+                </div>
+                <div class="col">
+                    <Card/>
+                </div>
+                </div>
+            </div>
+            </div>
+    </main>
+</template>
+
+<style scoped>
+</style>
